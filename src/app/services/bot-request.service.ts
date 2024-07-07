38fe5d018243600871adeffa1c601755e3505784
KirillArhipov <karhipov@truvisibility.com>
2024-07-07 23:00:25 +0300

    add env

--- src/app/services/bot-request.service.ts
+++ src/app/services/bot-request.service.ts
@@ -2,15 +2,14 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { map, Observable } from "rxjs";
 import { IChatBotResponse } from "../models/chat-bot-response.model";
+import { environment } from "../../environments/environment";
 
 @Injectable()
 export class BotRequestService {
-    private botMOKUrl = "../../assets/MOK.json";
-
     constructor(private http: HttpClient) { }
 
     public sendToChatBot(command: string): Observable<string> {
-        return this.http.get<any>(this.botMOKUrl).pipe(
+        return this.http.get<any>(`${environment.baseUrl}/assets/MOK.json`).pipe(
             map((chatBotResponse: IChatBotResponse) => {
                 let response = chatBotResponse.commands.find(x => x.command === command)?.response;
                 if (!response) {
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    baseUrl: 'https://dyinghexagon.github.io/rtlabs_test'
+};  
